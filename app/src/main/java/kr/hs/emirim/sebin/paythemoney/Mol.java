4433e6b47aaf1b3e96b88ec04fd128f0bedaf1ae
SeBin Nam <nsb2519@e-mirim.hs.kr>
2016-11-09 21:11:34 +0900

    #6 complete Mol and Douch

--- app/src/main/java/kr/hs/emirim/sebin/paythemoney/Mol.java
+++ app/src/main/java/kr/hs/emirim/sebin/paythemoney/Mol.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.sebin.paythemoney;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +15,10 @@ import android.widget.ViewFlipper;
 public class Mol extends AppCompatActivity implements View.OnClickListener{
     ViewFlipper vFlipper;
     TextView textresult;
-    Button butCheck,butCancel;
+    Button butStart,butStop;
     EditText editPerson;
-
-    int molMoney=0;
+    int perIndex;
+    int personNum=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,31 +28,30 @@ public class Mol extends AppCompatActivity implements View.OnClickListener{
         editPerson = (EditText) findViewById(R.id.edit_person);
         textresult=(TextView)findViewById(R.id.text_result);
         vFlipper =(ViewFlipper)findViewById(R.id.view_flip);
-        butCheck =(Button)findViewById(R.id.but_mol_check);
-        butCheck.setOnClickListener(this);
-        butCancel =(Button)findViewById(R.id.but_mol_delete);
-        butCancel.setOnClickListener(this);
-
-        //molMoney=Integer.parseInt(editPerson.getText().toString())
-    }
-
-    public void startMolRoulette(int molMoney){
-            //Toast.makeText(this," 더치 페이 결과:"+douchMoney+"원을 내주세요~",Toast.LENGTH_LONG).show()
-            int perIndex = (int)(Math.random()%molMoney+1);
-            textresult.setText(" * 걸린 사람: "+perIndex+"번째 사람!!");
-            Toast.makeText(this,"룰렛결과:"+perIndex+"번쨰 사람이 돈을 내주세요!",Toast.LENGTH_LONG).show();
+        butStart =(Button)findViewById(R.id.but_start);
+        butStart.setOnClickListener(this);
+        butStop =(Button)findViewById(R.id.but_stop);
+        butStop.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        String mMon=editPerson.getText().toString();
-        molMoney=Integer.parseInt(mMon);
+        String person=editPerson.getText().toString();
+
         switch(view.getId()){
-            case R.id.but_mol_check:
-                startMolRoulette(molMoney);
+            case R.id.but_start:
+                if(editPerson.equals("") || editPerson==null) Toast.makeText(this,"제대로 된 값을 입력해 주세요!",Toast.LENGTH_LONG).show();
+                else {
+                    personNum=Integer.parseInt(person);
+                    vFlipper.setFlipInterval(50);//viewFlipper에 넘겨지는 간격 설정 => 1000: 1초
+                    perIndex = (int)(Math.random()*personNum)+1;
+                    vFlipper.startFlipping();
+                }
                 break;
-            case R.id.but_mol_delete:
-                editPerson.setText("");
+            case R.id.but_stop:
+                vFlipper.stopFlipping();
+                Toast.makeText(this,perIndex+"번째 사람이 걸렸습니다!!",Toast.LENGTH_SHORT).show();
+                textresult.setText(" 걸린 사람: "+perIndex+"번째 사람!");
                 break;
         }
     }
